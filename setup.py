8870c1d992b43796eb88411e26efcc79dac18e24
pricheal <patrickricheal@gmail.com>
2020-11-24 17:12:56 -0500

    Get rid of dashes

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='db-cli',
+    name='dbcli',
     version='0.0.1',
     url='https://github.com/pricheal/db-cli',
     author='Patrick Richeal',
     author_email='patrickricheal@gmail.com',
     description='Universal database repl written in python with prompt toolkit, yet to be named',
     license='MIT',
-    packages=['db-cli'],
+    packages=['dbcli'],
     install_requires=[
         'prompt_toolkit'
     ],
